40e0300811d936f5c115bfbb77c47d87f66a5d30
Asranov <asranov0003@gmail.com>
2023-07-08 00:55:22 +0500

    ten tasks solved

--- script.ts
+++ script.ts
@@ -52,3 +52,50 @@ const largestNum = (nums: number[]): number => {
 
   return largest
 }
+
+//6.Find the second largest num in an array.
+const secondLargestNum = (nums: number[]): number => {
+  const sortedNumber = nums.sort((a, b) => b - a)
+  return sortedNumber[1]
+}
+
+//7.Check if two strings are anagrams.
+const areAnagrams = (str1: string, str2: string): boolean => {
+  const cleanStr1 = str1.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  const cleanStr2 = str2.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+  const sortedStr1 = cleanStr1.split("").sort().join("");
+  const sortedStr2 = cleanStr2.split("").sort().join("");
+
+  return sortedStr1 === sortedStr2;
+};
+
+//8.Count the number of occurrences of a specific character in a string.
+const countOccurrences = (str: string, char: string): number => {
+  let count = 0
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == char) {
+      count++
+    }
+  }
+
+  return count
+}
+
+//9.Remove duplicates from an array.
+const removeDuplicates = <T>(arr: T[]): T[] => {
+  return Array.from(new Set(arr));
+};
+
+//10.Check if an array is sorted in ascending order.
+const isSortedAscending = (arr: number[]): boolean => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
