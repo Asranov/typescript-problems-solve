62fcdb84d8898a5d85c41470edd2ce7c7eceb0cb
Asranov <asranov0003@gmail.com>
2023-07-08 00:21:23 +0500

    second task solved

--- script.ts
+++ script.ts
@@ -1,7 +1,6 @@
 // 50 TypeScript logical tasks
 
 //1.Write a function to check if a given number is prime.
-
 function isPrime(number: number): boolean {
   if (number <= 1) {
     return false
@@ -14,4 +13,19 @@ function isPrime(number: number): boolean {
   }
 
   return true
+}
+
+//2.Find the factorial of a given number.
+function factorial(number: number): number {
+  if (number === 0 || number === 1) {
+    return 1
+  }
+
+
+  let result = 1
+  for (let i = 1; i <= number; i++) {
+    result *= i
+  }
+
+  return result
 }
