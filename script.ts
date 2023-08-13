3c8c0cf2a812ba5d3c30106f6144b464d69602eb
Asranov <asranov0003@gmail.com>
2023-08-13 16:05:19 +0500

    findmissing number 2

--- script.ts
+++ script.ts
@@ -598,3 +598,15 @@ class Stack<T> {
   }
 }
 
+
+function findMissingNumber2(arr: number[]): number {
+  const n = arr.length + 1;
+  const expectedSum = (n * (n + 1)) / 2;
+  const actualSum = arr.reduce((sum, num) => sum + num, 0);
+  return expectedSum - actualSum;
+}
+
+const inputArray = [1, 2, 3, 5, 6];
+const missingNumber = findMissingNumber2(inputArray);
+console.log(`The missing number is: ${missingNumber}`);
+
