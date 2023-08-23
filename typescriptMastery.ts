8d6a4743ae7c5169aa9d6be95fafc9503a947859
Asranov <asranov0003@gmail.com>
2023-08-23 17:57:41 +0500

    arr reversal

--- typescriptMastery.ts
+++ typescriptMastery.ts
@@ -210,3 +210,17 @@ function findMax(arr: number[]): number {
 }
 
 console.log(findMax([5, 2, 9, 1, 5, 6])); // 9
+
+
+//Array Reversal
+function reverseArray(arr: any[]): void {
+  for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+    const temp = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = temp;
+  }
+}
+
+const numbers = [1, 2, 3, 4, 5];
+reverseArray(numbers);
+console.log(numbers); // [5, 4, 3, 2, 1]
