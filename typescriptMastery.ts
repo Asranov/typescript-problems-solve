92d58df6d98a33e7c77b3941ced34b2ecde9f6f8
Asranov <asranov0003@gmail.com>
2023-08-23 17:56:11 +0500

    fizzbuzz

--- typescriptMastery.ts
+++ typescriptMastery.ts
@@ -167,3 +167,21 @@ function isPalindrome(str: string): boolean {
 
 console.log(isPalindrome("racecar")); // true
 console.log(isPalindrome("hello"));   // false
+
+//FizzBuzz
+function fizzBuzz() {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+fizzBuzz();
+
