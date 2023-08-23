a8fb9a689635c0455c9793b0880e72297d0a64c5
Asranov <asranov0003@gmail.com>
2023-08-23 17:56:44 +0500

    factorial calculation

--- typescriptMastery.ts
+++ typescriptMastery.ts
@@ -185,3 +185,14 @@ function fizzBuzz() {
 
 fizzBuzz();
 
+//Factorial Calculation
+function factorial(n: number): number {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+console.log(factorial(5)); // 120
+console.log(factorial(0)); // 1
