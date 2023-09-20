507aa6d07badd0f0a55196894fb55efa752b96b8
Asranov Baxtinur <asranov0003@gmail.com>
2023-09-20 10:14:25 +0500

    name of types

--- tasks.ts
+++ tasks.ts
@@ -4,3 +4,8 @@
 //  1.Explain TypeScript and its use ?
 // TypeScript is an open - source programming language developed and maintained by Microsoft,
 //  which acts as a statically - typed superset of JavaScript.
+
+// 2.Can you tell the names of some of the built-in types in TypeScript?
+// In TypeScript, there are several built-in types available for developers to ensure safer,
+//  more accurate code. Some of the most common built-in types include:
+//  any, boolean, number, string, null, undefined, void etc.
