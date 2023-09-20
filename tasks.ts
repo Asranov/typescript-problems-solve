5fe70ce8b156a9e8237433c6e7ddf29e8acfdadc
Asranov Baxtinur <asranov0003@gmail.com>
2023-09-20 10:12:56 +0500

    explain ts and its use

--- tasks.ts
+++ tasks.ts
@@ -1,4 +1,6 @@
 //TypeScript interview-questions and answers
 //https://www.turing.com/interview-questions/typescript
 
-
+//  1.Explain TypeScript and its use ?
+// TypeScript is an open - source programming language developed and maintained by Microsoft,
+//  which acts as a statically - typed superset of JavaScript.
